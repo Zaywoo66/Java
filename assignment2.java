c921c5a3f962291d1353baeeedf6c3f6b55339e8
Alikhan <kokenovalihan07@gmail.com>
2025-12-29 08:32:15 +0500

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
+interface Playable {
+    void play();
+}
 
 class MediaItem {
     private String title;
@@ -16,21 +17,25 @@ class MediaItem {
         return title;
     }
 
-    public void setTitle(String title) {
+    public final void setTitle(String title) {
         this.title = title;
     }
 
-    @Override
     public String toString() {
         return "Title: " + title;
     }
 }
 
-class Song extends MediaItem {
+class Song extends MediaItem implements Playable {
     private int duration;
 
     public Song() {
         super();
+        this.duration = 0;
+    }
+
+    public Song(String title) {
+        super(title);
         this.duration = 0;
     }
 
@@ -43,8 +48,13 @@ class Song extends MediaItem {
         return duration;
     }
 
-    public void setDuration(int duration) {
+    public final void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing song: " + getTitle());
     }
 
     @Override
@@ -54,12 +64,12 @@ class Song extends MediaItem {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (!(obj instanceof Song)) return false;
-
-        Song other = (Song) obj;
-        return getTitle().equals(other.getTitle()) && duration == other.duration;
+        if (obj instanceof Song) {
+            Song other = (Song) obj;
+            return getTitle().equals(other.getTitle()) &&
+                   duration == other.duration;
+        }
+        return false;
     }
 
     @Override
@@ -70,52 +80,36 @@ class Song extends MediaItem {
 
 class Artist {
     private String name;
-    private String genre;
 
     public Artist() {
-        this.name = "";
-        this.genre = "";
+        name = "";
     }
 
-    public Artist(String name, String genre) {
+    public Artist(String name) {
         this.name = name;
-        this.genre = genre;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setName(String name) {
+    public final void setName(String name) {
         this.name = name;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
     }
 
     @Override
     public String toString() {
-        return "Artist: " + name + ", genre: " + genre;
+        return "Artist: " + name;
     }
 }
 
 class Playlist {
     private String name;
-    private int songsCount;
+    private Song[] songs;
 
-    public Playlist() {
-        this.name = "";
-        this.songsCount = 0;
-    }
-
-    public Playlist(String name, int songsCount) {
+    public Playlist(String name, Song[] songs) {
         this.name = name;
-        this.songsCount = songsCount;
+        this.songs = songs;
     }
 
     public String getName() {
@@ -123,74 +117,45 @@ class Playlist {
     }
 
     public int getSongsCount() {
-        return songsCount;
+        return songs.length;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSongsCount(int songsCount) {
-        this.songsCount = songsCount;
+    public void showSongs() {
+        for (int i = 0; i < songs.length; i++) {
+            System.out.println(songs[i]);
+        }
     }
 
     @Override
     public String toString() {
-        return "Playlist: " + name + ", songs: " + songsCount;
+        return "Playlist: " + name + ", songs: " + songs.length;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Song> songs = new ArrayList<>();
+        Song song1 = new Song("Shape of You", 240);
+        Song song2 = new Song("Blinding Lights", 200);
+        Song song3 = new Song("Believer", 210);
 
-        songs.add(new Song("Shape of You", 240));
-        songs.add(new Song("Blinding Lights", 200));
-        songs.add(new Song("Believer", 210));
+        Song[] songs = { song1, song2, song3 };
 
-        Artist artist1 = new Artist("Ed Sheeran", "Pop");
-        Artist artist2 = new Artist("The Weeknd", "R&B");
+        Artist artist = new Artist("Ed Sheeran");
 
-        Playlist playlist1 = new Playlist("My Favorites", 10);
-        Playlist playlist2 = new Playlist("Workout", 8);
+        Playlist playlist = new Playlist("My Favorites", songs);
 
-        System.out.println("Artists:");
-        System.out.println(artist1);
-        System.out.println(artist2);
+        System.out.println(artist);
+        System.out.println(playlist);
 
-        System.out.println("\nPlaylists:");
-        System.out.println(playlist1);
-        System.out.println(playlist2);
+        System.out.println("\nSongs in playlist:");
+        playlist.showSongs();
 
-        System.out.println("\nAll songs:");
-        for (Song song : songs) {
-            System.out.println(song);
-        }
+        System.out.println("\nPlay song:");
+        song1.play();
 
-        System.out.println("\nSongs longer than 210 sec:");
-        for (Song song : songs) {
-            if (song.getDuration() > 210) {
-                System.out.println(song);
-            }
-        }
-
-        System.out.println("\nSearch by title:");
-        for (Song song : songs) {
-            if (song.getTitle().equals("Believer")) {
-                System.out.println("Found: " + song);
-            }
-        }
-
-        Collections.sort(songs, (a, b) -> a.getDuration() - b.getDuration());
-
-        System.out.println("\nSorted by duration:");
-        for (Song song : songs) {
-            System.out.println(song);
-        }
-
-        Song testSong = new Song("Believer", 210);
         System.out.println("\nEquals test:");
-        System.out.println(songs.get(2).equals(testSong));
+        Song testSong = new Song("Believer", 210);
+        System.out.println(song3.equals(testSong));
     }
 }
